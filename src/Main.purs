4ff84a0852b4970e77cadc9d269523ddcc65c58c
Alex Kelley <adkelley@mac.com>
2020-08-17 12:09:08 -0700

    refactor: watch in package.json, add: comments in Main.purs

--- src/Main.purs
+++ src/Main.purs
@@ -13,12 +13,12 @@ import Foreign (Foreign, unsafeFromForeign)
 import Effect (Effect)
 import Effect.Console (log)
 
-
+-- | Return the value of a cell given its range in 'A1' format
 getValueFromRange :: String -> GASEff Foreign
 getValueFromRange range = 
   app >>= getActiveSheet >>= getRange range >>= getValue
   
+-- | Print out the value of a cell given its range in 'A1' format
 main :: Effect Unit
 main = do
-  --value <- getValueFromRange "A1"
   getValueFromRange "A1" >>= \x -> log $ unsafeFromForeign x
